bfe3d34b94096a9673d0e58cda82c06dea4c72ba
salekmichal1 <michal.salek@microsoft.wsei.edu.pl>
2024-05-30 22:58:23 +0200

    testing server api

--- src/App.tsx
+++ src/App.tsx
@@ -28,19 +28,53 @@ function App() {
     localStorage.setItem('tasks', JSON.stringify([]));
   }
 
-  // const fetchData = async function () {
-  //   try {
-  //     const testApi = await fetch('http://localhost:3000/res');
-  //     if (!testApi.ok) {
-  //       throw Error(testApi.statusText);
-  //     }
-  //     const testApiData = await testApi.json()
-  //     console.log(testApiData.mess);
-  //   } catch (err) {
-  //     console.error(err);
-  //   }
-  // };
-  // fetchData()
+  const fetchData = async function () {
+    try {
+      const testApi = await fetch('http://localhost:3000');
+      if (!testApi.ok) {
+        throw Error(testApi.statusText);
+      }
+      const testApiData = await testApi.json();
+      console.log(testApiData.message);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  fetchData();
+
+  const postData = async function () {
+    try {
+      const testApi = await fetch('http://localhost:3000/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username: 'test', password: 'test' }),
+      });
+      if (!testApi.ok) {
+        throw Error(testApi.statusText);
+      }
+
+      const testApiData = await testApi.json();
+      console.log(testApiData.token);
+
+      const protectedApi = await fetch(
+        'http://localhost:3000/protected/10/1000',
+        {
+          headers: {
+            Authorization: `Bearer ${testApiData.token}`,
+          },
+        }
+      );
+
+      const protectedApiData = await protectedApi.json();
+      console.log(protectedApiData.message);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  postData();
 
   return (
     <div className="App">
