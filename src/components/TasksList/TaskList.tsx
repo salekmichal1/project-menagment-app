363bfb369120db103cab08d0c94340800233342e
salekmichal1 <michal.salek@microsoft.wsei.edu.pl>
2024-06-19 15:49:00 +0200

    working on TaskList

--- src/components/TasksList/TaskList.tsx
+++ src/components/TasksList/TaskList.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Task } from '../../model/Task';
 import './TaskList.css';
 import {
@@ -13,6 +13,8 @@ import {
   TableSortLabel,
   Collapse,
   IconButton,
+  Box,
+  Typography,
 } from '@mui/material';
 
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
@@ -22,10 +24,68 @@ interface TaskListProps {
   data: Task[];
 }
 
+function Row(props: { row: Task }) {
+  const { row } = props;
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <React.Fragment>
+      <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
+        <TableCell>
+          <IconButton
+            aria-label="expand row"
+            size="small"
+            onClick={() => setOpen(!open)}>
+            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+          </IconButton>
+        </TableCell>
+        <TableCell component="th" scope="row">
+          {row.name}
+        </TableCell>
+        <TableCell>{row.priority}</TableCell>
+        <TableCell>
+          {row.expectedEndDate
+            ? row.expectedEndDate.toDate().toLocaleDateString()
+            : 'N/A'}
+        </TableCell>
+        <TableCell>
+          {row.expectedEndDate.toDate().toLocaleDateString()}
+        </TableCell>
+        <TableCell>
+          {row.expectedEndDate.toDate().toLocaleDateString()}
+        </TableCell>
+        <TableCell>
+          {row.endDate
+            ? row.expectedEndDate.toDate().toLocaleDateString()
+            : 'N/A'}
+        </TableCell>
+        <TableCell>{row.state}</TableCell>
+        <TableCell>{row.pinedUser}</TableCell>
+        {/* <TableCell>
+          <Button onClick={() => handleEdit(task)}>Edit</Button>
+        </TableCell> */}
+      </TableRow>
+      <TableRow>
+        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={9}>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <Box sx={{ margin: 1 }}>
+              <Typography variant="h6" gutterBottom component="div">
+                Description
+              </Typography>
+              <Typography gutterBottom component="div">
+                {row.description}
+              </Typography>
+            </Box>
+          </Collapse>
+        </TableCell>
+      </TableRow>
+    </React.Fragment>
+  );
+}
+
 export default function TaskList({ data }: TaskListProps) {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [selectedTask, setSelectedTask] = useState<Task | null>(null);
-  const [open, setOpen] = useState(false);
 
   const handleEdit = (task: Task) => {
     setSelectedTask(task);
@@ -46,15 +106,12 @@ export default function TaskList({ data }: TaskListProps) {
 
   return (
     <TableContainer component={Paper}>
-      <Button onClick={handleAdd}>Add Task</Button>
-      <Table>
+      <Table aria-label="collapsible table">
         <TableHead>
           <TableRow>
+            <TableCell />
             <TableCell>
               <TableSortLabel>Name</TableSortLabel>
-            </TableCell>
-            <TableCell>
-              <TableSortLabel>description</TableSortLabel>
             </TableCell>
             <TableCell>
               <TableSortLabel>priority</TableSortLabel>
@@ -77,41 +134,11 @@ export default function TaskList({ data }: TaskListProps) {
             <TableCell>
               <TableSortLabel>pinedUser</TableSortLabel>
             </TableCell>
-            {/* Add more table cells for each property */}
           </TableRow>
         </TableHead>
         <TableBody>
           {tasks.map((task, index) => (
-            <TableRow key={index}>
-              <TableCell>{task.name}</TableCell>
-              <TableCell>
-                {task.description} vv vuygiyv2uyuy uyg teguygegwg
-                gyiuwyugywyguyg gygyugywygryurwyugygrf gyw gfgyuwgyuegyufwyiu
-              </TableCell>
-              <TableCell>{task.priority}</TableCell>
-              <TableCell>
-                {task.expectedEndDate
-                  ? task.expectedEndDate.toDate().toLocaleDateString()
-                  : 'N/A'}
-              </TableCell>
-              <TableCell>
-                {task.expectedEndDate.toDate().toLocaleDateString()}
-              </TableCell>
-              <TableCell>
-                {task.expectedEndDate.toDate().toLocaleDateString()}
-              </TableCell>
-              <TableCell>
-                {task.endDate
-                  ? task.expectedEndDate.toDate().toLocaleDateString()
-                  : 'N/A'}
-              </TableCell>
-              <TableCell>{task.state}</TableCell>
-              <TableCell>{task.pinedUser}</TableCell>
-              {/* Add more table cells for each property */}
-              <TableCell>
-                <Button onClick={() => handleEdit(task)}>Edit</Button>
-              </TableCell>
-            </TableRow>
+            <Row key={index} row={task} />
           ))}
         </TableBody>
       </Table>
